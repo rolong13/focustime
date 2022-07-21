354780b0076ff9459574ae0e4dd566c52052ea49
Roger Long <docrogerlong@gmail.com>
2022-07-21 11:34:03 -0500

    android fix

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     padding: spacing.lg,
-    justifyContent: 'top',
+    justifyContent: 'center',
     flexDirection: 'row',
   },
 });
